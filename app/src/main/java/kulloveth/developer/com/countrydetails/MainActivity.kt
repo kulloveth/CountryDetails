4c0a91eca49569c78b048a3a549a5dad828a7f3a
kulloveth <kulloveth@gmail.com>
2020-02-24 21:26:25 +0100

    Initial commit

---
+++ app/src/main/java/kulloveth/developer/com/countrydetails/MainActivity.kt
@@ -0,0 +1,12 @@
+package kulloveth.developer.com.countrydetails
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
