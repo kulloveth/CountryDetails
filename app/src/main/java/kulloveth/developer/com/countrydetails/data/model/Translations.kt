a5bd1f5b5404e187e2a7ab161dc8613062336b6a
kulloveth <kulloveth@gmail.com>
2020-02-28 15:06:31 +0100

    model class added for translation

---
+++ app/src/main/java/kulloveth/developer/com/countrydetails/data/model/Translations.kt
@@ -0,0 +1,16 @@
+package kulloveth.developer.com.countrydetails.data.model
+
+import java.io.Serializable
+
+data class Translations(
+    val de: String,
+    val es: String,
+    val fr: String,
+    val ja: String,
+    val it: String,
+    val br: String,
+    val pt: String,
+    val nl: String,
+    val hr: String,
+    val fa: String
+) : Serializable
