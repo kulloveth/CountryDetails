9802f4d4eb004e7b0324163f9d551dc51f7b5ac5
kulloveth <kulloveth@gmail.com>
2020-02-28 16:15:10 +0100

    service class created to fetch data

---
+++ app/src/main/java/kulloveth/developer/com/countrydetails/api/RetrofitService.kt
@@ -0,0 +1,27 @@
+package kulloveth.developer.com.countrydetails.api
+
+import io.reactivex.Single
+import kulloveth.developer.com.countrydetails.data.model.CountryDetails
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+interface RetrofitService {
+
+    @GET("rest/v2/all/")
+    fun fetchCharacterName(): Single<Response<CountryDetails>>
+
+
+    companion object {
+        fun getRetrofitInstance(): RetrofitService {
+            val retrofit: Retrofit = Retrofit.Builder()
+                .baseUrl("https://restcountries.eu/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
+            return retrofit.create(RetrofitService::class.java)
+        }
+    }
+}
