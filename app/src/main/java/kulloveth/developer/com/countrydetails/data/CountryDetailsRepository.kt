2a13ddfa824b8d98faa70c68a48734f4720dbf6b
kulloveth <kulloveth@gmail.com>
2020-02-28 14:39:14 +0100

    model class added for currency

---
+++ app/src/main/java/kulloveth/developer/com/countrydetails/data/CountryDetailsRepository.kt
@@ -0,0 +1,4 @@
+package kulloveth.developer.com.countrydetails.data
+
+class CountryDetailsRepository {
+}
---
+++ app/src/main/java/kulloveth/developer/com/countrydetails/data/model/Currency.kt
@@ -0,0 +1,8 @@
+package kulloveth.developer.com.countrydetails.data.model
+
+import java.io.Serializable
+
+class Currency(
+    val code: String,
+    val name: String, val symbol: String
+) : Serializable
