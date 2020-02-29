e88b97dff161ecf06c551776f449a634b44c6d99
kulloveth <kulloveth@gmail.com>
2020-02-29 18:16:25 +0100

    setup Activity to display countries and there flag

--- app/src/main/java/kulloveth/developer/com/countrydetails/MainActivity.kt
+++ app/src/main/java/kulloveth/developer/com/countrydetails/MainActivity.kt
@@ -5,24 +5,27 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import kulloveth.developer.com.countrydetails.ui.MainAdapter
 import kulloveth.developer.com.countrydetails.ui.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
-
+    val adapter = MainAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        countryRv.layoutManager = StaggeredGridLayoutManager(2, requestedOrientation)
+        countryRv.adapter = adapter
+
         val viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.fetchCountrys().observe(this, Observer {
-          it.forEach {
-              Log.d("nnnn","" + it.name )
-             //var names = " "
-             // names += it.name + "\n"
-             // nametv.append(names)
-          }
+            it.forEach {
+                Log.d("nnnn", "" + it.name)
+            }
+            adapter.submitList(it)
         })
     }
 }
