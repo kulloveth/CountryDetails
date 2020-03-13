8b01e2950f61fcb8ab433f5749ad89204f1ca6c9
kulloveth <kulloveth@gmail.com>
2020-03-13 13:17:10 +0100

    created languages fragment

---
+++ app/src/main/java/kulloveth/developer/com/countrydetails/ui/details/LanguagesFragment.kt
@@ -0,0 +1,26 @@
+package kulloveth.developer.com.countrydetails.ui.details
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import kulloveth.developer.com.countrydetails.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class LanguagesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_languages, container, false)
+    }
+
+
+}
