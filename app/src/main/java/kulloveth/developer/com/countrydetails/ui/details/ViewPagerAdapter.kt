8e2d20416c2d56e384d950f6e65d5214e8073e85
kulloveth <kulloveth@gmail.com>
2020-03-13 13:17:44 +0100

    created pager adapter

--- app/src/main/java/kulloveth/developer/com/countrydetails/ui/details/ViewPagerAdapter.kt
+++ app/src/main/java/kulloveth/developer/com/countrydetails/ui/details/ViewPagerAdapter.kt
@@ -4,7 +4,7 @@ import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class ViewPagerAdapter(fragment:Fragment):FragmentStateAdapter(fragment) {
-    override fun getItemCount(): Int = 1
+    override fun getItemCount(): Int = 2
 
     override fun createFragment(position: Int): Fragment {
         return TranslationsFragment()
